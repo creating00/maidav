4bd31a3093bc026e72f2b4c702a19680733e7f84
creating00 <creatingsr@gmail.com>
2026-04-06 20:57:22 -0300

    foto mobile

--- src/main/java/com/sales/maidav/web/controller/web/GlobalModelAttributes.java
+++ src/main/java/com/sales/maidav/web/controller/web/GlobalModelAttributes.java
@@ -8,7 +8,13 @@ import org.springframework.beans.factory.ObjectProvider;
 import org.springframework.security.authentication.AnonymousAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.multipart.MaxUploadSizeExceededException;
+import org.springframework.web.servlet.FlashMap;
+import org.springframework.web.servlet.support.RequestContextUtils;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ModelAttribute;
 
 @ControllerAdvice(annotations = Controller.class)
@@ -42,5 +48,25 @@ public class GlobalModelAttributes {
                 && !(authentication instanceof AnonymousAuthenticationToken)
                 && authentication.getName() != null
                 && !authentication.getName().isBlank();
+    }
+
+    @ExceptionHandler(MaxUploadSizeExceededException.class)
+    public String handleMaxUploadSizeExceeded(MaxUploadSizeExceededException ex,
+                                              HttpServletRequest request,
+                                              HttpServletResponse response) {
+        FlashMap flashMap = RequestContextUtils.getOutputFlashMap(request);
+        flashMap.put("formError", "La imagen es demasiado pesada. Proba con una foto mas liviana o comprimida.");
+        RequestContextUtils.getFlashMapManager(request).saveOutputFlashMap(flashMap, request, response);
+
+        String referer = request.getHeader("Referer");
+        if (referer != null && !referer.isBlank()) {
+            return "redirect:" + referer;
+        }
+
+        String requestUri = request.getRequestURI();
+        if (requestUri != null && requestUri.matches(".*/products/\\d+$")) {
+            return "redirect:" + requestUri + "/edit";
+        }
+        return "redirect:/products/new";
     }
 }
